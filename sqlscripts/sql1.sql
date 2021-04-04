6b0c2b3d7339724963e54c6e9003ac18160556bf
harsha270501 <harshavardhinibalakrishnan@gmail.com>
2021-04-04 21:56:14 +0530

    sql changes

--- sqlscripts/sql1.sql
+++ sqlscripts/sql1.sql
@@ -51,7 +51,7 @@ insert into course_list values('15CSE301','COA',
                                'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing',
                                'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing');
 
--- drop table course_list; 
+-- drop table course_list;
 
 create table course_faculty(course_id varchar(50) references course_list(course_code),
 						    batch int, 
@@ -65,8 +65,7 @@ insert into course_faculty values('15CSE313',2018,'CSE','A','12301');
 insert into course_faculty values('15CSE313',2018,'CSE','B','13301');
 -- drop table course_faculty;
 
-select course_id,batch,dept,section from course_faculty where faculty_id='12301';
-
+-- select course_id,batch,dept,section from course_faculty where faculty_id='12301';
 -- *************************************************************************************************
 -- student-info
 -- *************************************************************************************************
@@ -154,9 +153,9 @@ insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18008',10,9,10,13,12.5,11.25,40,48,47,18,17);
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18009',8,9,7,13,14.5,11.25,14,50,47,18,20);
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18010',10,9,9,13,12.5,15,14,48,50,20,19);
-select * from course_15CSE313_2018_CSE_A_student_academic_info;
+-- select * from course_15CSE313_2018_CSE_A_student_academic_info;
 
-select * from course_15CSE313_2018_CSE_A_student_academic_info where roll_number='CB.EN.U4CSE18001';
+-- select * from course_15CSE313_2018_CSE_A_student_academic_info where roll_number='CB.EN.U4CSE18001';
 
 -- drop table assessment_list;
 -- drop table student_academic_info;
