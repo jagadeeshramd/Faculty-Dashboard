bcf303fac91447591dc9eb80c16010cd0953e719
HarishK501 <harishcse18501@gmail.com>
2021-04-05 01:33:36 +0530

    Updated sql code

--- sqlscripts/sql1.sql
+++ sqlscripts/sql1.sql
@@ -45,13 +45,14 @@ create table course_list(course_code varchar(15) primary key,
                          course_details varchar(100),
                          course_outcome varchar(100),
                          course_eval varchar(100));
+                         
 insert into course_list values('15CSE301','COA',
 							   'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing',
                                'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing',
                                'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing',
                                'https://drive.google.com/file/d/1OUFXHFi65onyEmYfk7tvurHgaZ4bij5T/view?usp=sharing');
 
--- drop table course_list;
+-- drop table course_list; 
 
 create table course_faculty(course_id varchar(50) references course_list(course_code),
 						    batch int, 
@@ -65,7 +66,8 @@ insert into course_faculty values('15CSE313',2018,'CSE','A','12301');
 insert into course_faculty values('15CSE313',2018,'CSE','B','13301');
 -- drop table course_faculty;
 
--- select course_id,batch,dept,section from course_faculty where faculty_id='12301';
+select course_id,batch,dept,section from course_faculty where faculty_id='12301';
+
 -- *************************************************************************************************
 -- student-info
 -- *************************************************************************************************
@@ -153,9 +155,9 @@ insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18008',10,9,10,13,12.5,11.25,40,48,47,18,17);
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18009',8,9,7,13,14.5,11.25,14,50,47,18,20);
 insert into course_15CSE313_2018_CSE_A_student_academic_info values('CB.EN.U4CSE18010',10,9,9,13,12.5,15,14,48,50,20,19);
--- select * from course_15CSE313_2018_CSE_A_student_academic_info;
+select * from course_15CSE313_2018_CSE_A_student_academic_info;
 
--- select * from course_15CSE313_2018_CSE_A_student_academic_info where roll_number='CB.EN.U4CSE18001';
+select * from course_15CSE313_2018_CSE_A_student_academic_info where roll_number='CB.EN.U4CSE18001';
 
 -- drop table assessment_list;
 -- drop table student_academic_info;
@@ -216,3 +218,10 @@ insert into tests(name, date,time, instructions, course)
 select * from tests;
 
 -- drop table tests;
+
+-- *************************************************************************************************
+-- assignments
+-- *************************************************************************************************
+
+create table assignments(id int primary key AUTO_INCREMENT, name varchar(100), date varchar(10), time varchar(5), instructions varchar(1000), course varchar(50));
+select * from assignments;
