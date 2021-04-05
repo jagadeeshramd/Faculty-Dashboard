622aaa8e7cd6f0194e5812514af5e0b26aca6903
harsha270501 <harshavardhinibalakrishnan@gmail.com>
2021-04-05 12:26:50 +0530

    updated testcases

--- spec/test1.spec.js
+++ spec/test1.spec.js
@@ -11,6 +11,7 @@ describe("server",()=>{
         server.close();
     });
     
+    //positive case for updatecourse
     describe("GET /",()=>{
         var data={};
         beforeAll((done)=>{
@@ -20,9 +21,7 @@ describe("server",()=>{
                 done();
             });
         });
-        it("Status 200",() =>{
-            expect(data.status).toBe(200);
-        });
+        
         it("updatecoursetab",() =>{
             b=data.body;
             b=JSON.parse(b);
@@ -31,6 +30,26 @@ describe("server",()=>{
         
     });
 
+    //negative case for updatecourse
+    describe("GET /",()=>{
+        var data={};
+        beforeAll((done)=>{
+            req.get("http://localhost:3000/updatecoursetab?course=15CSE3132018_CSE_A",(error,response,body)=>{
+                data.status=response.statusCode;
+                data.body=body;
+                done();
+            });
+        });
+        
+        it("updatecoursetab",() =>{
+            b=data.body;
+            b=JSON.parse(b);
+            expect(b["status"]).toBe(false);
+        });
+        
+    });
+
+    //positive testcase for get_quiz_marks
     describe("GET /",()=>{
         var data={};
         beforeAll((done)=>{
@@ -47,6 +66,27 @@ describe("server",()=>{
             b=data.body;
             b=JSON.parse(b);
             expect(b["resp"]).toBe(true);
+        });
+        
+    });
+
+    //negative testcase for get_quiz_marks
+    describe("GET /",()=>{
+        var data={};
+        beforeAll((done)=>{
+            req.get("http://localhost:3000/get_quiz_marks?course=15CSE313_2018_CSE_A&rollno=CB.EN.U4CSE18011",(error,response,body)=>{
+                data.status=response.statusCode;
+                data.body=body;
+                done();
+            });
+        });
+        it("Status 200",() =>{
+            expect(data.status).toBe(200);
+        });
+        it("quizmark",() =>{
+            b=data.body;
+            b=JSON.parse(b);
+            expect(b["resp"]).toBe(false);
         });
         
     });
