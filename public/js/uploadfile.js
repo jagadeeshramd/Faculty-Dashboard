fc05c0baa2837bea4485164d2464b09fa8993b73
harsha270501 <61742450+harsha270501@users.noreply.github.com>
2021-06-12 22:54:21 +0530

    duplication resolved

--- public/js/uploadfile.js
+++ public/js/uploadfile.js
@@ -11,22 +11,6 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i of cookies) {
-            const cookie = i.trim();
-
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 const csrftoken = getCookie('csrftoken');
     
 var selectedFile;
