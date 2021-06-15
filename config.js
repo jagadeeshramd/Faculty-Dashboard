57b5f6f2b9d7ae436b1f466a386d4e80f14ddc27
Harish K <60997526+HarishK501@users.noreply.github.com>
2021-06-16 00:47:01 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-exports.adminEmail = "admin@fd.com";
-exports.adminPass = "qwertyui";
-exports.GAccount = "harry.18501@gmail.com";
-exports.GPassword = "#Temp@Harry123";
-exports.SQLUser = "root";
-exports.SQLPass = "password";
